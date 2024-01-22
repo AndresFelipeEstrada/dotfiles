05ce7173d309519146bf3e8ccdad6465d81a75a3
Andres <andresfe1396@gmail.com>
2024-01-21 20:36:06 -0500

    add: options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-local opt = vim.opt
 
+local opt = vim.opt
+vim.scriptencoding = "utf-8"
+opt.encoding = "utf-8"
+opt.fileencoding = "utf-8"
 opt.wrap = true
+opt.autoindent = true
+opt.breakindent = true
+opt.title = true
+opt.wildignore:append({ "*/node_modules/*" })
